5598db92b3fc4a8c5de4d01b8c29e5bd476a6e65
baek <dkf1912@gmail>
2022-04-18 11:31:12 +0900

    Posting 댓글추가, 조회수 Counting - BJS

--- community/views.py
+++ community/views.py
@@ -41,6 +41,27 @@ def post(request): # 글 조회 -> 글 ID가 넘어온다는 가정 하에
         data = JSONParser().parse(request)
         
         obj = Post.objects.get(id = data['id'])
-        serialzer = PostingSerializer(obj)
-        return JsonResponse(serialzer.data, safe = False)
+        obj.view_count += 1 # 조회수 증가
+        obj.save()
+
+        comments = Comment.objects.filter(post_id = data['id']) # 해당 글의 댓글 가져오기
+
+        data = {}
+        
+        for index, comment in enumerate(comments):
+            tmp = {}
+            tmp['id'] = comment.id
+            tmp['content'] = comment.content
+            tmp['create_dt'] = comment.create_dt
+            tmp['content'] = comment.post_id
+            data['comment' + str(index)] = tmp
+        
+        data['id'] = obj.id
+        data['title'] = obj.title
+        data['content'] = obj.content
+        data['create_dt'] = obj.create_dt
+        data['like'] = obj.like
+        data['view_count'] = obj.view_count
+    
+        return JsonResponse({"RESULT" : data}, status = 200)
 
