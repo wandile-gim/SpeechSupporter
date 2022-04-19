c631c1d633b40913533bbe7f5d20c93c24203389
김동현 <kdh@gimdonghyeon-ui-MacBookAir.local>
2022-04-19 11:15:06 +0900

    Add Drag Function // kdh

--- bigproject/src/MainPage.js
+++ bigproject/src/MainPage.js
@@ -1,7 +1,11 @@
 import { useState, useRef } from 'react';
 import './App.css';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 const MainPage = () => {
+    const x = useMotionValue(0);
+    const background = useTransform(x, [-100, 0, 100], ['#ff008c', '#7700ff', 'rgb(230, 255, 0)']);
+
     const [loginState, setLoginState] = useState(false);
     const [idInputState, setIdInputState] = useState(false);
     const [passwordInputState, setPasswordInputState] = useState(false);
@@ -34,36 +38,16 @@ const MainPage = () => {
             {loginState ? (
                 <>
                     <div className='container'>
-                        <div className='neckless__container'>
-                            <div className='neckless_front'></div>
-                            <div className='neckless_back'></div>
-                        </div>
-                        <div className='App__rotate'>
+                        <motion.div className='App__rotate' drag='x' dragConstraints={{ left: 0, right: 0 }} style={{ x }}>
                             <div className='top'></div>
                             <div className='hole'></div>
                             <div className='logo_container'>
                                 <img className='logo' src='aivle.png' />
                             </div>
                             <div className='img__container'>
-                                <form id='hidden'>
-                                    <input type='text' placeholder='Please Fill Your Id' />
-                                    <input type='password' placeholder='Please Fill Your Password' />
-                                    <input type='submit' value='Login' />
-                                </form>
-                                <div className='regist__container hidden'>
-                                    <a href=''>
-                                        <span className='span'>Join Us</span>
-                                    </a>
-                                </div>
-                                <div className='link__container hidden'>
-                                    <img style={{ width: '30px' }} src='./google.png' />
-                                    <a href=''>
-                                        <span className='span'>Login With Google</span>
-                                    </a>
-                                </div>
                                 <img src='' />
                             </div>
-                        </div>
+                        </motion.div>
                     </div>
                 </>
             ) : (
