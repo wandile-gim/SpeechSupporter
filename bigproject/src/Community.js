af5328400539d3e36513a4bb7e8273e82803602c
HyunHo <gh5768@hanmail.net>
2022-04-21 00:19:44 +0900

    community 화면 작은 수정

--- bigproject/src/Community.js
+++ bigproject/src/Community.js
@@ -3,23 +3,23 @@ import styles from "./Community.module.css";
 const data = [
   {
     id: 15,
-    user: "wonjae",
+    user: "김원재",
     title: "안녕",
     category: null,
     content: "작성자는?",
     create_dt: "2022-04-19T21:21:11.541097+09:00",
-    like: 0,
-    view_count: 0,
+    like: 1,
+    view_count: 2,
   },
   {
     id: 14,
-    user: "채걸",
+    user: "김동현",
     title: "안녕안녕 나는 어드민~",
     category: 1,
     content: "어드민 포스팅이다",
     create_dt: "2022-04-19T21:17:40.232225+09:00",
-    like: 0,
-    view_count: 0,
+    like: 3,
+    view_count: 4,
   },
   {
     id: 13,
@@ -28,43 +28,43 @@ const data = [
     category: 1,
     content: "짧은 글을 담은 게시글",
     create_dt: "2022-04-19T20:05:43.683558+09:00",
-    like: 0,
-    view_count: 0,
+    like: 5,
+    view_count: 6,
   },
   {
     id: 12,
-    user: "누구게",
-    title: "안녕안녕 나는 누구누구야",
+    user: "이현호",
+    title: "안녕안녕 나는 누구야",
     category: null,
     content: "로그인 누구야qq",
     create_dt: "2022-04-19T13:33:05.081578+09:00",
-    like: 2851,
-    view_count: 10573,
+    like: 7,
+    view_count: 8,
   },
   {
     id: 11,
-    user: 1,
-    title: "안녕",
+    user: "이소연",
+    title: "Hello",
     category: null,
     content: "로그인 누구야asas",
     create_dt: "2022-04-19T13:32:46.521494+09:00",
-    like: 0,
-    view_count: 0,
+    like: 9,
+    view_count: 10,
   },
   {
     id: 10,
-    user: 1,
-    title: "안녕",
+    user: "홍길동",
+    title: "1루수가 누구야",
     category: null,
     content: "로그인 누구야",
     create_dt: "2022-04-19T13:31:11.045278+09:00",
-    like: 0,
-    view_count: 0,
+    like: 16545,
+    view_count: 416543,
   },
   {
     id: 9,
-    user: 1,
-    title: "안녕",
+    user: "LHH",
+    title: "2루수는 뭐야",
     category: null,
     content: "로그인 누구야",
     create_dt: "2022-04-19T13:30:52.915929+09:00",
@@ -73,8 +73,8 @@ const data = [
   },
   {
     id: 8,
-    user: 1,
-    title: "안녕",
+    user: "어나니머스",
+    title: "33333333333333333333",
     category: null,
     content: "로그인 누구야",
     create_dt: "2022-04-19T13:28:31.703395+09:00",
@@ -84,7 +84,7 @@ const data = [
   {
     id: 7,
     user: 1,
-    title: "안녕",
+    title: "하기싫다아아ㅏ아아아",
     category: null,
     content: "로그인 누구야",
     create_dt: "2022-04-19T13:24:26.689477+09:00",
@@ -158,9 +158,7 @@ const curpage = 1;
 const Community = () => {
   return (
     <div className={styles.container}>
-      <a href="">
-        <img className={styles.logo} src="aivle.png" />
-      </a>
+      <img src="aivle.png" className={styles.logo} />
       <nav className={styles.navigator}>
         <img className={styles.profile} src="king.png" />
         <h5 id="id">아이디</h5>
@@ -176,6 +174,14 @@ const Community = () => {
         <a href="">다른게시판</a>
         <a href="http://localhost:3000/main">메인으로</a>
       </nav>
+      <div className={styles.post_title}>
+        <div id={styles.number}></div>
+        <div id={styles.title}>제목</div>
+        <div id={styles.user_id}>ID</div>
+        <div id={styles.date}>날짜</div>
+        <div id={styles.like}>좋아요</div>
+        <div id={styles.view}>조회수</div>
+      </div>
       <div className={styles.post}>
         {data.map((data) => {
           return (
@@ -205,7 +211,10 @@ const Community = () => {
           </a>
         </div>
         <div className={styles.page}>
-          <navi></navi>
+          <navi>1 2 3 4 5 6 7 8 9 10</navi>
+        </div>
+        <div className={styles.search}>
+          <input placeholder="검색" />
         </div>
       </div>
     </div>
